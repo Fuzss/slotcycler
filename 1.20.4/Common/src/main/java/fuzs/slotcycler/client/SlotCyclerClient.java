226d981cd20ace957a608495c3b015fead4cd4a9
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-13 13:56:17 +0100

    bump library

--- 1.20.4/Common/src/main/java/fuzs/slotcycler/client/SlotCyclerClient.java
+++ 1.20.4/Common/src/main/java/fuzs/slotcycler/client/SlotCyclerClient.java
@@ -3,11 +3,14 @@ package fuzs.slotcycler.client;
 import fuzs.hotbarslotcycling.api.v1.client.HotbarCyclingProvider;
 import fuzs.hotbarslotcycling.api.v1.client.SlotCyclingProvider;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import net.minecraft.world.entity.player.Player;
 
 public class SlotCyclerClient implements ClientModConstructor {
 
     @Override
     public void onClientSetup() {
-        SlotCyclingProvider.registerProvider(player -> new HotbarCyclingProvider(player.getInventory()));
+        SlotCyclingProvider.registerProvider((Player player) -> {
+            return new HotbarCyclingProvider(player.getInventory());
+        });
     }
 }
