05eacdefcc4e640dc5c1ff91f89195b56d46a101
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-07-14 08:55:07 +0200

    full 1.21 port

--- 1.21/NeoForge/src/main/java/fuzs/slotcycler/neoforge/SlotCyclerNeoForge.java
+++ 1.21/NeoForge/src/main/java/fuzs/slotcycler/neoforge/SlotCyclerNeoForge.java
@@ -2,16 +2,12 @@ package fuzs.slotcycler.neoforge;
 
 import fuzs.puzzleslib.api.core.v1.ModConstructor;
 import fuzs.slotcycler.SlotCycler;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
 
 @Mod(SlotCycler.MOD_ID)
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class SlotCyclerNeoForge {
 
-    @SubscribeEvent
-    public static void onConstructMod(final FMLConstructModEvent evt) {
+    public SlotCyclerNeoForge() {
         ModConstructor.construct(SlotCycler.MOD_ID, SlotCycler::new);
     }
 }
