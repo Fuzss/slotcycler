2244cd9c37a7aea8889298153c84d9a68572823e
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-11 22:54:12 +0200

    full 1.21.5 port

--- 1.21.5/Common/src/main/java/fuzs/slotcycler/client/SlotCyclerClient.java
+++ 1.21.5/Common/src/main/java/fuzs/slotcycler/client/SlotCyclerClient.java
@@ -2,8 +2,8 @@ package fuzs.slotcycler.client;
 
 import fuzs.hotbarslotcycling.api.v1.client.HotbarCyclingProvider;
 import fuzs.hotbarslotcycling.api.v1.client.SlotCyclingProvider;
-import fuzs.puzzleslib.api.client.core.v1.ClientAbstractions;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import fuzs.puzzleslib.api.config.v3.ConfigHolder;
 import fuzs.slotcycler.SlotCycler;
 import net.minecraft.world.entity.player.Player;
 
@@ -11,7 +11,7 @@ public class SlotCyclerClient implements ClientModConstructor {
 
     @Override
     public void onConstructMod() {
-        ClientAbstractions.INSTANCE.registerConfigScreenFactory(SlotCycler.MOD_ID, "hotbarslotcycling");
+        ConfigHolder.registerConfigurationScreen(SlotCycler.MOD_ID, "hotbarslotcycling");
     }
 
     @Override
